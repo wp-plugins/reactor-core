48cdcc4f85d4171126f11bc30545555296f8ea60
modemlooper <modemlooper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-09 23:18:17 +0000

    tagged version 0.0.8
    git-svn-id: https://plugins.svn.wordpress.org/reactor-core/trunk@1064249 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reactor-core.php
+++ reactor-core.php
@@ -4,7 +4,7 @@ Plugin Name: Reactor: Core
 Plugin URI: http://reactor.apppresser.com
 Description: Core plugin for Reactor App Builder.
 Text Domain: reactor
-Version: 0.0.7
+Version: 0.0.8
 Author: Reactor Team
 Author URI: http://reactor.apppresser.com
 License: GPLv2
@@ -26,13 +26,14 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-define( 'REACTOR_CORE_VERSION', '0.0.7' );
+define( 'REACTOR_CORE_VERSION', '0.0.8' );
 define( 'REACTOR_API_VERSION', 'v1' );
-define( 'REACTOR_PLUGIN_PATH', __FILE__ );
+define( 'REACTOR_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'REACTOR_PLUGIN_URL', trailingslashit( plugins_url( null , __FILE__ ) ) );
 
 class Reactor_Core {
 
-	const VERSION = '0.0.7';
+	const VERSION = '0.0.8';
 	public static $instance = null;
 	public static $dir_path;
 	public static $dir_url;
@@ -78,6 +79,7 @@ class Reactor_Core {
 		}
 
 		// reactor api
+		include_once( self::$dir_path . 'inc/api/' . REACTOR_API_VERSION . '/AppPresser_Reactor_Template_Override.php' );
 		include_once( self::$dir_path . 'inc/api/' . REACTOR_API_VERSION . '/AppPresser_Reactor_Media_API.php' );
 		include_once( self::$dir_path . 'inc/media/media-uploader.php' );
 
